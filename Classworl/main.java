0314108f03c589c88f9d01e707fe2a956e4e36b7
Joywei2003 <133044119+Joywei2003@users.noreply.github.com>
2024-06-27 15:02:41 -0400

    Create main.java

---
+++ Classworl/main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println
+                ("CST3613 - Application Development with Databases" +
+                 "Spring 2024 - M & W @ 12pm" +
+                 "Sam Shahidullah - sshahidullah@citytech.cuny.edu"
+                );
+         System.out.println
+                ("Droped Class");
+    }
+}
